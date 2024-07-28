9d615ef02ab5f6725a632a5ab0eb66a1d6598d35
Zhimeng Xin <162425451+zhimengXin@users.noreply.github.com>
2024-07-28 18:39:37 +0800

    Create run_voc_gfsod.sh

---
+++ run_voc_gfsod.sh
@@ -0,0 +1,10 @@
+gpu_id=$
+echo "gpu_id: ${gpu_id}"
+for shot in 1 3 10
+do 
+    config_file="configs/voc/ecea_gfsod_r101_novel1_${shot}shot_seed1.yaml"
+    CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --config-file ${config_file} \
+        --output_dir "base1/${shot}shot_gfsod" \
+        --num-gpus  4 \
+        --opts MODEL.WEIGHTS "voc/model_reset_surgery.pth" \
+done
