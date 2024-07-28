49c3dbf5c26a7e1538d5295eb5e748be583fffd1
Zhimeng Xin <162425451+zhimengXin@users.noreply.github.com>
2024-07-28 18:40:55 +0800

    Update run_voc_fsod.sh

--- run_voc_fsod.sh
+++ run_voc_fsod.sh
@@ -1,6 +1,6 @@
 gpu_id=$4
 echo "gpu_id: ${gpu_id}"
-for shot in 1 3 10
+for shot in 1 2 3 5 10
 do 
     config_file="configs/voc/ecea_gfsod_r101_novel1_${shot}shot_seed1.yaml"
     CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --config-file ${config_file} \
